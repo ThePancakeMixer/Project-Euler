4be976939a8854b1f4e1a542ab175ee4ddfd7f9b
ThePancakeMixer <nikeshp96@gmail.com>
2016-07-20 00:43:50 -0700

    Revert "Add files via upload"
    This reverts commit d4e5b16ae11cc09bd54b22213e5cf2c3a46bd308.

--- smallestMultiple.cpp
+++ smallestMultiple.cpp
@@ -4,26 +4,27 @@ using namespace std;
 
 
 
-int sumSquareDifference(){
+int smallestPositive(){
+    bool isDivisible=false; //flag to see if input is evenly divisible
+    int smallestPositive = 20;//var to hold smallest positive divisible by 1-20
 
-    int sumS=0;//int to store sum of squares
-    int sqS=0;//int to store square of sum
-    int answer; //variable to store answer
+    loopback:
 
-    for(int i=1;i<=100;i++){//loop through 1-100
-        sqS+=i;//add i to sqS
-        sumS+= (i*i);//add square of i to sum of Squares
+    smallestPositive++; //check next number
+    while(!isDivisible){ //while no number is found
+        isDivisible=true; //innocent until proven guilty
+        for(int i =1; i<=20; i++){ //loop through 1-20
+            if(smallestPositive % i != 0){ //if the input is not evenly divisible by 1-20
+                isDivisible=false; //set isDivisible to false;
+                goto loopback; //try again
+            }
+        }
     }
-
-    sqS*=sqS;//square squareofSums
-    answer = sumS-sqS; //store answer in variable
-    if(answer<0) //flip sign if negative
-        answer*=-1;
-    return answer;//return |sumofSquares - squareofSums|
+    return smallestPositive; //return smallest positive divisible by 1-20
 }
 
 int main(){
-    cout<<sumSquareDifference();
+    cout<<smallestPositive(); //returns smallestPositive divisible 1-20
 }
 
 
