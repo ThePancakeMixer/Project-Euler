87f493f35b276de05514d2bb2672e7918f6e6b37
ThePancakeMixer <nikeshp96@gmail.com>
2016-07-20 03:25:06 -0700

    returns largest product of n numbers in 1000 number sequence
    returns largest product of n numbers in 1000 number sequence

---
+++ largestProductinSeries.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+char charArray[] = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+int numArray[1000];
+//store 1000 number sequence in char array
+//array to store chars into ints
+
+long long int largestNproduct(int n) {//returns largest product of n numbers in 1000 number sequence
+    long long int adNums[n]; //create array to store sequence of n Adjacent numbers
+    long long int tempProd;
+    long long int product=0; //variable to store largest product
+    int i;
+    for(int start=0;start<1001-n;start++){ //iterate through number blob
+        for( i=0;i<n;i++){ //create sequences of n-digit arrays
+            adNums[i]=(long long)numArray[start+i];
+        }
+        tempProd=1;
+        for(i=0;i<n;i++) {
+            tempProd*=adNums[i]; //multiple all numbers in array and store in tempProd
+        }
+        if(tempProd>product) { //if tempProd is the largest generated product store it
+            product = tempProd;
+        }
+    }
+    return product; //return the largest generated product
+
+}
+
+void toNumArray(char* a, int* b){ //converts char array a to int array b
+    for(int i = 0; i<1000; i++){
+        numArray[i] = (charArray[i] - '0'); //change char to int
+    }
+}
+
+int main() {
+    toNumArray(charArray, numArray);
+    cout<<largestNproduct(13);
+}
